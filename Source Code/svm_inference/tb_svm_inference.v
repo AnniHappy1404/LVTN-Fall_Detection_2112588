fbb683ad7c33d0b57e06219b68df9f28518bbcd3
AnniHappy1404 <tuanpham2601KD@gmail.com>
2025-05-22 13:56:17 +0700

    Update tb_svm_inference.v

--- Source Code/svm_inference/tb_svm_inference.v
+++ Source Code/svm_inference/tb_svm_inference.v
@@ -2,55 +2,60 @@
 
 module tb_svm_inference;
 
-    reg clk;
-    reg reset_n;
-    reg start;
-    reg signed [31:0] feature_mean;
-    reg signed [31:0] feature_std;
+    reg clk = 0;
+    reg reset_n = 0;
+    reg start = 0;
+
+    reg signed [15:0] feature0, feature1, feature2, feature3, feature4, feature5, feature6;
     wire fall_detected;
+    wire done;
+
+    // Clock 50MHz
+    always #10 clk = ~clk;
 
     // Instantiate DUT
     svm_inference dut (
         .clk(clk),
         .reset_n(reset_n),
         .start(start),
-        .feature_mean(feature_mean),
-        .feature_std(feature_std),
-        .fall_detected(fall_detected)
+        .feature_0(feature0),  // Sửa thành feature_0
+        .feature_1(feature1),  // Sửa thành feature_1
+        .feature_2(feature2),  // Sửa thành feature_2
+        .feature_3(feature3),  // Sửa thành feature_3
+        .feature_4(feature4),  // Sửa thành feature_4
+        .feature_5(feature5),  // Sửa thành feature_5
+        .feature_6(feature6),  // Sửa thành feature_6
+        .fall_detected(fall_detected),
+        .done(done)
     );
 
-    // Clock generation
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk;
-    end
-
-    reg [15:0] test_vectors [0:6]; // 7 đặc trưng
+    reg [15:0] hex_data [0:6];
 
     initial begin
-        $display("=== BẮT ĐẦU TEST ===");
+        $display("=== MO PHONG PHAT HIEN TE NGA ===");
+        $readmemh("D:\\LVTN_21112588_FINAL_1\\svm_inference\\F15_SA01_R01_features.hex", hex_data);
 
-        // Sửa đường dẫn tại đây
-        $readmemh("D:/LVTN_21112588_FINAL_1/svm_inference/test1_vectors.hex", test_vectors);
-
-        feature_mean = {{16{test_vectors[0][15]}}, test_vectors[0]};
-        feature_std  = {{16{test_vectors[1][15]}}, test_vectors[1]};
+        feature0 = hex_data[0];
+        feature1 = hex_data[1];
+        feature2 = hex_data[2];
+        feature3 = hex_data[3];
+        feature4 = hex_data[4];
+        feature5 = hex_data[5];
+        feature6 = hex_data[6];
 
         reset_n = 0;
-        start = 0;
-        #20;
+        #50;
         reset_n = 1;
-        #20;
 
+        #20;
         start = 1;
-        #10;
+        #20;
         start = 0;
 
-        wait(fall_detected === 1 || fall_detected === 0);
-        #100;
+        wait (done == 1);
+        #20;
 
-        $display("Fall detected: %b", fall_detected);
-        $stop;
+        $display("FALL DETECTED = %b", fall_detected);
+        $finish;
     end
-
 endmodule
